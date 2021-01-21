5376dde9c207f580999cf20e48daadaec1eb2600
allawa1 <alexiaallaway@gmail.com>
2021-01-21 18:55:09 -0500

    commitb4deploy

--- routes/Events.js
+++ routes/Events.js
@@ -9,17 +9,18 @@ router.get('/', (req, res)=>{
     .then(data => res.json(data))
 }); 
 
+router.get('/categories', (req,res)=>{
+
+})
 router.post('/', (req, res)=>{
     const event = new Event({
         eventID: req.body.eventID,
         title: req.body.title,
         description: req.body.description,
+        date: new Date(req.body.date).toDateString(),
         location: req.body.location,
         host: req.body.host,
-        formName: req.body.formName,
-        formDate: req.body.formDate,
-        formLocation: req.body.formLocation,
-        formDetails: req.body.formDetails
+        tag: req.body.tag
     });
     event.save()
     .then(data => res.json(data))
--- server.js
+++ server.js
@@ -49,37 +49,9 @@ app.get('/heartbeat', (req, res) => {
     })
 }) 
 
-app.get('/categories', (req, res) => {
-    //console.log(process.env.EB_TOKEN)
-    let bearer = 'Bearer ' + process.env.EB_TOKEN; 
-    let url = "https://www.eventbriteapi.com/v3/categories/"; 
-    fetch(url, {
-        method: 'GET', 
-        headers: {
-            "Authorization" : bearer, 
-            'Content-Type': 'application/json',
-        }
-    })
-        .then(result => result.json())
-        .then(data => res.json(data))
-        .catch(error => res.json({error: error}))
-}) 
 
-app.get('/popular-events', (req, res) => {
-    let bearer = 'Bearer ' + process.env.EB_TOKEN; 
-    let url = "https://www.eventbriteapi.com/v3/venues/"; 
-    fetch(url,
-        {
-        method: 'GET', 
-        headers: {
-            "Authorization" : bearer, 
-            'Content-Type': 'application/json',
-        }
-    })
-        .then(result => result.json())
-        .then(data => res.json(data))
-        .catch(error => res.json({error: error}))
-}) 
+
+
 
 app.get('/ongoing-events', (req, res) => {
     res.json({
